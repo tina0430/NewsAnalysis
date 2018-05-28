8d49d7c986fd8c1355716239d39113b6e9b60be1
tina0430 <leejihee950430@gmail.com>
2018-05-28 16:43:13 +0900

    refin_new_day 인풋값 변경

--- newsRefining/refining.py
+++ newsRefining/refining.py
@@ -2,11 +2,11 @@ import json
 from newsRefining.functions import refin_news
 import time
 
-def refin_new_day(date):
+def refin_new_day(file_name):
     news = ''
     start_time = time.time()
 #     file_name = '../news_'+date+'.json'
-    with open(date, 'r', encoding='utf-8') as f:
+    with open(file_name, 'r', encoding='utf-8') as f:
         jdata = json.load(f)
         cnt = len(jdata)
      
@@ -22,6 +22,7 @@ def refin_new_day(date):
     #         print(jdata['news' + str(i)]['contents'])
     print(jdata)
     return jdata
+#     data 가 인풋일 경우
 #     file_name = '../news_'+date+'_refin.json'
 #     with open(file_name, 'w', encoding='utf-8') as f:
 #         json.dump(jdata, f, ensure_ascii=False, indent="\t")
